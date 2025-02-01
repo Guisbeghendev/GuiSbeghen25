55648d0fcbda05616e77dac3cab783b7c14bcb37
guisbeghen <guisbeghendev@gmail.com>
2025-02-01 16:43:08 -0300

    commit 0.21

--- guisbeghen/wsgi.py
+++ guisbeghen/wsgi.py
@@ -1,23 +1,6 @@
-"""
-WSGI config for guisbeghen project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guisbeghen.settings')
 
-def application(environ, start_response):
-    status = '200 OK'
-    output = b'Hello, WSGI is working!'
-
-    response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-
-    return [output]
+application = get_wsgi_application()
